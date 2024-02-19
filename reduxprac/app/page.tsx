6d0ad0f4f7be43cc5ea87b46a0a1459154575ca9
Shashwat Singh <shashwat123student@gmail.com>
2024-02-19 22:14:36 +0530

    Basic view actions and state is working

--- reduxprac/app/page.tsx
+++ reduxprac/app/page.tsx
@@ -2,6 +2,7 @@
 
 import { useSelector,  useDispatch} from 'react-redux'
 import { RootState } from '@/provider/redux/store'
+import {IncreaseNumber} from "@/provider/redux/ChangeNumber";
 export default function Home() {
 
     const state = useSelector((state: RootState) => state.ChangeNumber.number);
@@ -14,6 +15,8 @@ export default function Home() {
       <h1>
         This is the home page
       </h1>
+        <button onClick={() => dispatch(IncreaseNumber())}>Change Number</button>
+        {state}
     </div>
   );
 }
